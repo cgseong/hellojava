c4cc88ee30d522720f6a387db92e6c40ca2d047d
Changgyu Seong <47137166+cgseong@users.noreply.github.com>
2020-07-20 19:10:08 +0900

    자바소스

--- src/Lesson02/Exam01.java
+++ src/Lesson02/Exam01.java
@@ -2,14 +2,18 @@ package Lesson02;
 
 public class Exam01 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args){
 		
-		int x,y,z;
-			
-		x=100;
-		y=200;
-		z=x+y;
-		System.out.println(x+" + "+y+" = "+z);
+		int x;
+		x=(int)127.5;
+		System.out.println(x);
+		String str = "æ»≥Á«œººø‰.";
+		char ch ='A';
+		System.out.println(str.length());
+		System.out.println((int)ch);
+		boolean b=false;
+		System.out.println(b);
+		System.out.println(7==7);
 
 	}
 
