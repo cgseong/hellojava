3ae8ba3ed5fbaa36a6e8f3f3236d57307a46dff8
Changgyu Seong <47137166+cgseong@users.noreply.github.com>
2020-07-20 11:32:48 +0900

    첫번쨰 소스

---
+++ src/Exam01.java
@@ -0,0 +1,15 @@
+/*
+ * 자바프로그래밍
+ */
+public class Exam01 {
+	//main 메소드
+	public static void main(String[] args) {
+		int x,y,z;
+		
+		x=10;
+		y=20;
+		z=x+y;
+		System.out.println(x+" + "+y+" = "+z);
+		
+	}
+}
