c1b923d525b582abebd9b820dad3e946627a5445
Changgyu Seong <47137166+cgseong@users.noreply.github.com>
2020-07-20 11:40:31 +0900

    자바소스

---
+++ src/Lesson01/Exam01.java
@@ -0,0 +1,17 @@
+package Lesson01;
+
+/*
+ * 자바프로그래밍
+ */
+public class Exam01 {
+	//main 메소드
+	public static void main(String[] args) {
+		int x,y,z;
+//변수 대입		
+		x=10;
+		y=20;
+		z=x+y;
+		System.out.println(x+" + "+y+" = "+z);
+		
+	}
+}
--- src/Lesson02/Exam01.java
+++ src/Lesson02/Exam01.java
@@ -1,15 +1,16 @@
-/*
- * 자바프로그래밍
- */
+package Lesson02;
+
 public class Exam01 {
-	//main 메소드
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int x,y,z;
-//변수 대입		
-		x=10;
-		y=20;
+		//변수 대입		
+		x=100;
+		y=200;
 		z=x+y;
 		System.out.println(x+" + "+y+" = "+z);
-		
+
 	}
+
 }
