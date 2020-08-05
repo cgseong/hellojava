3c67df1c3e352932c51fef362cbac33f39f855cb
Changgyu Seong <47137166+cgseong@users.noreply.github.com>
2020-08-05 14:35:16 +0900

    자바소스

---
+++ src/Lesson03/MathclassDemo.java
@@ -0,0 +1,19 @@
+package Lesson03;
+
+public class MathclassDemo {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);	// PI
+		System.out.println((int)(Math.random()*100));	// 0 ~ 99
+		System.out.println(Math.ceil(10.1));      // 11.0
+		System.out.println(Math.floor(10.9));     // 10.0
+		System.out.println(Math.round(10.4));     // 10
+		System.out.println(Math.round(10.5));     // 11
+		System.out.println(Math.max(10, 11));      // 11
+		System.out.println(Math.min(10, 11));      // 10
+		System.out.println((int)Math.pow(5, 2)); // 25
+		System.out.println((int)Math.sqrt(25));  // 5	
+
+	}
+
+}
