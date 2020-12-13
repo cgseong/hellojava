dfd4eb145964ccc3e76c42fa3ec168517f3c0e76
Changgyu Seong <47137166+cgseong@users.noreply.github.com>
2020-12-13 17:58:45 +0900

    Update FileIOEx03.java

--- src/Lesson11/FileIOEx03.java
+++ src/Lesson11/FileIOEx03.java
@@ -1,7 +1,6 @@
 package Lesson11;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class FileIOEx03 {
@@ -9,11 +8,11 @@ public class FileIOEx03 {
 	public static void main(String[] args) throws IOException {
 		double sales;
 		
-		Scanner salesFile = new Scanner(new File("D:/Temp/sales.txt"));
+		Scanner salesFile = new Scanner(new File("C:/Temp/sales.txt"));
 		
 		while (salesFile.hasNext()) { 
 			sales = salesFile.nextDouble();
-			System.out.printf("$%,.2f\n", sales);
+			System.out.println(sales);
 		}
 		salesFile.close();
 	}
