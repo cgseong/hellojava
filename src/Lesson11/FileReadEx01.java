98d2bdd3d21ffafd35dcb880e255d416fc7481ea
Changgyu Seong <47137166+cgseong@users.noreply.github.com>
2020-12-13 18:00:14 +0900

    Update FileReadEx01.java

--- src/Lesson11/FileReadEx01.java
+++ src/Lesson11/FileReadEx01.java
@@ -6,10 +6,12 @@ import java.io.IOException;
 public class FileReadEx01 {
 
 	public static void main(String[] args) throws IOException {
-		 byte[] b = new byte[1024];
-	     FileInputStream input = new FileInputStream("D:/Temp/out1.txt");
-	     input.read(b);
-	     System.out.println(new String(b));
-	     input.close();
+		File file = new File("C:\\Temp\\test.txt");
+		FileReader fr = new FileReader(file);
+		int ch = 0; 
+		while((ch = fr.read()) != -1) { 
+			System.out.print((char)ch); 
+		}
+		fr.close();
 	}
 }
